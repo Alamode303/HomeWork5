e72422c8df8284763c957a9a048677652e67dacf
Artem <kashcheev.94@bk.ru>
2020-10-05 18:58:21 +0300

    5_2 корректировка

--- task5_2/Task5_2.java
+++ task5_2/Task5_2.java
@@ -8,12 +8,7 @@ public class Task5_2 {
 Найти количество вхождений одной строки в другую.
 Например, строка "дом" встречается в строке "дом домик домой одомашненный" 4 раза
  */
-/*
-        string1 = "Java P y t h o n";
-        String[] str = string1.split(" ");
 
-        System.out.println(string1.startsWith("JJD"));
-        */
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите слова:");
         String words = scanner.nextLine();
@@ -21,9 +16,7 @@ public class Task5_2 {
         String word1 = scanner.nextLine();
         String[] words2 = words.split(" ");
         int j = 0;
-        for (int i = 0; i < words2.length; i++) {
-            if (words2[i].contains(word1)) j++;
-        }
+        for (String s : words2) if (s.contains(word1)) j++;
         System.out.println("Количество повторений: " + j);
     }
 }
